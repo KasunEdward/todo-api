c309f1556f31d28829d17646d444a3f37e6a67a7
Kasun Edward <kasunedward48@gmail.com>
2024-05-17 22:15:47 +0800

    feat: add connection and models

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-//server api
+require("dotenv").config();
+require("./db/connection");
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const {log} = require("mercedlogger"); // import mercedlogger's log function
+const cors = require("cors"); // import cors
+
+//DESTRUCTURE ENV VARIABLES WITH DEFAULT VALUES
+const {PORT = 3000} = process.env;
+
+// Create Application Object
+const app = express();
+
+// GLOBAL MIDDLEWARE
+app.use(cors()); // add cors headers
+app.use(morgan("tiny")); // log the request for debugging
+app.use(express.json()); // parse json bodies
+
+// ROUTES AND ROUTES
+app.get("/", (req, res) => {
+    res.send("this is the test route to make sure server is working");
+})
+
+// APP LISTENER
+app.listen(PORT, () => log.green("SERVER STATUS", `Listening on port ${PORT}`));
